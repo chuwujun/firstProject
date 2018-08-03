78285f5d65ba09ce5cd1eafd59acb7cb0baf2681
chuwujun <1579916124@qqmcin>
2018-08-03 16:09:17 +0800

    menu

--- tkinter_learn/014-04-Menu_split.py
+++ tkinter_learn/014-04-Menu_split.py
@@ -1,6 +1,15 @@
 from Tkinter import *
+#https://blog.csdn.net/jcodeer/article/details/1811321
+def printItem():
+    print "add separator"
 
 root=Tk()
-
+menuBar=Menu(root)
+filemenu=Menu(menuBar,tearoff=0)
+for k in ['PHP','Python','C']:
+    filemenu.add_command(label=k,command=printItem)
+    filemenu.add_separator()
+menuBar.add_cascade(label='Language',menu=filemenu)
+root['menu']=menuBar
 
 mainloop()
