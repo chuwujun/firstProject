0fdf060cddc1a72eb00b87d594a79401b563d3d5
chuwujun <1579916124@qqmcin>
2018-08-03 15:14:51 +0800

    spinbox and menu

--- tkinter_learn/014-01-Menu.py
+++ tkinter_learn/014-01-Menu.py
@@ -1,6 +1,13 @@
 from Tkinter import *
+#https://blog.csdn.net/jcodeer/article/details/1811321
+def print_hello():
+    print "hello"
 
 root=Tk()
+menuBar=Menu(root)
+for items in ['PHP','Python','C','Java']:
+    menuBar.add_command(label=items,command=print_hello)
+root['menu']=menuBar
 
 mainloop()
 
