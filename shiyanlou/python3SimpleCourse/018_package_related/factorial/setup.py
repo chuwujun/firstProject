d9daf9c8178fb0a6e9bd3bd6785fd293aa31f49c
chuwujun <1579916124@qq.com>
2018-08-06 14:16:14 +0800

    zheng li

---
+++ shiyanlou/python3SimpleCourse/018_package_related/factorial/setup.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+
+from setuptools import find_packages,setup
+
+setup(name='factorial',
+      version='0.1',
+      description='A test module for our book',
+      long_description='Factorial module',
+      platform=["Linux"],
+      author='shiyanlou',
+      author_email="support@shiyanlou.com",
+      url='https://www.shiyanlou.com/courses/596',
+      license='MIT',
+      packages=find_packages()
+)
