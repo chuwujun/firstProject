35a181f5d2db58323a45fd11bf8dd1edb381a054
chuwujun <1579916124@qq.com>
2018-10-12 10:42:20 +0800

    check init of C file

---
+++ checkinit/Code/d
@@ -0,0 +1,11 @@
+fdsalj
+
+    int b-5
+int c=6
+        int d
+int e(fdsfj)
+
+float cc
+
+
+    fdsafsdfa int a
---
+++ checkinit/main.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import re
+
+#get file name list
+def get_file_list(dirpath):
+    file_list=[]
+    all_list=os.listdir(dirpath)
+    for i in range(0,len(all_list)):
+        path=os.path.join(dirpath,all_list[i])
+        if os.path.isfile(path):
+            file_list.append(path)
+        else:
+            file_list.extend(get_file_list(path))
+    return file_list
+
+
+
+normal_type=['int','float','double','char','long','short']
+current_path=os.path.abspath(sys.argv[0])
+dirlist=current_path.split(os.sep)
+code_dir=(os.sep).join(dirlist[0:-1])+os.sep+'Code'
+file_list=get_file_list(code_dir)
+for file in file_list:
+    line_num=0
+    file_content=[]
+    f=open(file,'r')
+    file_content=f.readlines()
+    f.close()
+    for i in range(0,len(file_content)):
+        line=file_content[i].replace('\n','')
+        line=line.strip()
+        line_num=line_num+1
+        #print line
+        for one_type in normal_type:
+            #exclude function
+            if re.match(one_type,line)  and line.find('(')==-1 :
+                if line.find('=')==-1:
+                    print "["+one_type+" error] line_num is :"+str(line_num)+" in "+file
