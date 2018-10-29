aecd3852f169d9c48bfc40329463ad051d0bbed5
chuwujun <1579916124@qq.com>
2018-10-29 16:10:59 +0800

    update

--- checkinit/main.py
+++ checkinit/main.py
@@ -15,12 +15,19 @@ def get_file_list(dirpath):
     return file_list
 
 
-
+file_type=['.c','.h']
 normal_type=['int','float','double','char','long','short']
 current_path=os.path.abspath(sys.argv[0])
 dirlist=current_path.split(os.sep)
 code_dir=(os.sep).join(dirlist[0:-1])+os.sep+'Code'
 file_list=get_file_list(code_dir)
+clear_list=[]
+for one in file_list:
+    if ''.join(one[-2:]) not in file_type:
+        clear_list.append(one)
+for clear in clear_list:
+    if clear in file_list:
+        file_list.remove(clear)
 for file in file_list:
     line_num=0
     file_content=[]
