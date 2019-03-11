c2a9f8ef6e22ca70e2479b08aa0cf715caf82825
chuwujun <1579916124@qq.com>
2019-03-11 23:44:45 +0800

    spider

--- get_github.py
+++ get_github.py
@@ -3,19 +3,26 @@ import scrapy
 
 class getSpider(scrapy.Spider):
     name="getSpider"
-    @property
-    def start_urls(self):
-        url1="https://github.com/shiyanlou?before=Y3Vyc29yOnYyOpK5MjAxNy0wNi0wN1QwNjoyMToxMCswODowMM4FkpVn&tab=repositories"
-        url2="https://github.com/shiyanlou?after=Y3Vyc29yOnYyOpK5MjAxNy0wNi0wN1QwNjoyMToxNiswODowMM4FkpXb&tab=repositories"
-        url3="https://github.com/shiyanlou?after=Y3Vyc29yOnYyOpK5MjAxNS0wMS0yNlQxNjoxNzo1MyswODowMM4Bx3_0&tab=repositories"
-        url4="https://github.com/shiyanlou?after=Y3Vyc29yOnYyOpK5MjAxNC0xMS0yNFQxNTowMDoxNyswODowMM4BnPdj&tab=repositories"
-        return (url for url in [url1,url2,url3,url4])
 
     @property
+    def start_urls(self):
+        return [
+        "https://github.com/shiyanlou?before=Y3Vyc29yOnYyOpK5MjAxNy0wNi0wN1QwNjoyMToxMCswODowMM4FkpVn&tab=repositories",
+        "https://github.com/shiyanlou?after=Y3Vyc29yOnYyOpK5MjAxNy0wNi0wN1QwNjoyMToxNiswODowMM4FkpXb&tab=repositories",
+        "https://github.com/shiyanlou?after=Y3Vyc29yOnYyOpK5MjAxNS0wMS0yNlQxNjoxNzo1MyswODowMM4Bx3_0&tab=repositories",
+        "https://github.com/shiyanlou?after=Y3Vyc29yOnYyOpK5MjAxNC0xMS0yNFQxNTowMDoxNyswODowMM4BnPdj&tab=repositories"
+        ]
+
+#    def parse(self,response):
+#        for i in response.css('li.col-12'):
+#            yield {
+#                'name': i.css('a::text').extract_first().strip(),
+#                'update_time': i.css('relative-time::attr(datetime)').extract_first()
+#            }
     def parse(self,response):
-        listb=response.xpath('//relative-time/@datetime').extract()
-        lista=response.xpath('//h3/a/text()').re('\\n\s*(.*)')
-        for one in lista:
-            name=one
-            update_time=listb[lista.index(one)]
-            yield{"name":name,"update_time":update_time}
+#        for one in response.xpath('//div[contains(@class, "col-9")]'):
+        for one in response.xpath('//div[@class="col-9 d-inline-block"]'):
+            yield {
+                'name':one.xpath('.//h3/a/text()').extract_first().strip(),
+                'update-time':one.xpath('.//relative-time/@datetime').extract_first()
+            }
